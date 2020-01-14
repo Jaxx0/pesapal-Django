de13ac10f2fc0958c8f1ac7afd0299213dd85925
Jackson <jackson.kitili@gmail.com>
2020-01-14 15:34:51 +0300

    Update pesapal_ops3.py

--- felonzo/payment/pesapal_ops3.py
+++ felonzo/payment/pesapal_ops3.py
@@ -2,12 +2,13 @@ import urllib.request
 
 from . import pesapal_processor3
 
+pesapal_processor3.consumer_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+pesapal_processor3.consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'
+pesapal_processor3.testing = False
+
 
 def post_transaction(Reference, FirstName, LastName, Email, PhoneNumber, Description, Amount, Type):
-    pesapal_processor3.consumer_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
-    pesapal_processor3.consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    pesapal_processor3.testing = False
-
+    
     post_params = {
         'oauth_callback': 'http://32c65eea.ngrok.io/payment/oauth_callback/'
     }
@@ -29,8 +30,6 @@ def post_transaction(Reference, FirstName, LastName, Email, PhoneNumber, Descrip
 
 
 def get_detailed_order_status(merchant_reference, transaction_tracking_id):
-    pesapal_processor3.consumer_key = 'nRZXsEDVT2uMetIZxnusQPOA4zHyBtlV'
-    pesapal_processor3.consumer_secret = 'UzKwzyN9GbXgSNC8HKcMHrJusME='
 
     post_params = {
         'pesapal_merchant_reference': merchant_reference,
@@ -43,8 +42,6 @@ def get_detailed_order_status(merchant_reference, transaction_tracking_id):
 
 
 def get_payment_status(merchant_reference, transaction_tracking_id):
-    pesapal_processor3.consumer_key = 'nRZXsEDVT2uMetIZxnusQPOA4zHyBtlV'
-    pesapal_processor3.consumer_secret = 'UzKwzyN9GbXgSNC8HKcMHrJusME='
 
     post_params = {
         'pesapal_merchant_reference': merchant_reference,
